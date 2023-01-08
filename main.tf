31e3a7e67885aeebca176d94a604327b2cb560cb
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-08 23:47:26 +0400

    added run action11

---
+++ main.tf
@@ -0,0 +1,12 @@
+# provider "aws" {
+#   access_key = "${var.aws_access_key}"
+#   secret_key = "${var.aws_secret_key}"
+#   region     = "us-east-1"
+# }
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-rayah77776667"
+  
+}
